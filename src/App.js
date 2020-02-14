68bb90b053f9208692c61e997383efc28de8df5c
Michael Fluch <michael.fluch@mail.com>
2020-02-14 11:23:46 +0100

    cors installed in backend, and fetch of movie list from backend api implemented (59).

--- src/App.js
+++ src/App.js
@@ -4,17 +4,28 @@ import MovieList from './components/movie-list';
 
 class App extends Component {
 
-  movies = ['titanic', 'avatar', 'mission impossible'];
+  state = {
+    movies: []
+  }
 
   componentDidMount() {
-    // fetch data
+    // fetch data when App is mounted
+    fetch('http://localhost:8000/api/movies/', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Token 86fc7a6fe9f8564a562ada960cf751af1fcace11'
+      }
+    }).then( resp => resp.json())
+      //.then( res => console.log(res))
+      .then( res => this.setState({movies: res}))
+      .catch( error => console.log(error))
   }
 
   render() {
     return (
       <div className="App">
         <h1>Movie Rater</h1>
-        <MovieList movies={this.movies} />
+        <MovieList movies={this.state.movies} />
       </div>
     );
 
